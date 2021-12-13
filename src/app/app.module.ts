6efdb722e855375f76f37968ef9116e40aa2aa24
jonatas marques <marquesjon@gmail.com>
2021-12-12 22:59:37 -0300

    add tema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { InicioComponent } from './inicio/inicio.component';
     RodapeComponent,
     CadastrarComponent,
     EntrarComponent,
-    InicioComponent
+    InicioComponent,
+    TemaComponent
   ],
   imports: [
     BrowserModule,
